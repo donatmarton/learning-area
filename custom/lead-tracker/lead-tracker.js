0d96dc66698233f70c6e1b062fd496d4814214fc
Márton Donát Nagy <donat@donatmarton.com>
2021-12-05 14:02:07 +0100

    create a leads tracker firefox extension

---
+++ custom/lead-tracker/lead-tracker.js
@@ -0,0 +1,48 @@
+const saveButton = document.querySelector('.input-button');
+const deleteButton = document.querySelector('.delete-button');
+const input = document.querySelector('.input-field');
+const leadList = document.querySelector('.lead-list');
+
+const storedLeads = JSON.parse(localStorage.getItem('myLeads'));
+let myLeads = storedLeads ? storedLeads : [];
+renderLeads()
+
+saveButton.addEventListener('click', saveLead);
+function saveLead(event) {
+  const lead = input.value;
+  myLeads.push(lead);
+  input.value = "";
+  input.focus();
+  addLead(lead);
+  updateLocalStorage();
+}
+
+function renderLeads() {
+  while(leadList.firstChild) {
+    leadList.removeChild(leadList.firstChild);
+  }
+  for(let i = 0; i < myLeads.length; i++) {
+    addLead(myLeads[i]);
+  }
+}
+
+function addLead(lead) {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  li.appendChild(a);
+  a.textContent = lead;
+  a.href = "https://" + lead;
+  a.target = "_blank";
+  leadList.appendChild(li);
+}
+
+function updateLocalStorage() {
+  const stringifiedLeads = JSON.stringify(myLeads);
+  localStorage.setItem('myLeads', stringifiedLeads);
+}
+
+deleteButton.addEventListener('dblclick', () => {
+  myLeads = [];
+  updateLocalStorage();
+  renderLeads();
+});
